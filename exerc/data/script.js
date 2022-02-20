d7edf2e2f663c123e74dc8692fd800ebe22fc514
Henrique-Navarro <henriquenavarromorais@gmail.com>
2022-02-20 18:07:41 -0300

    exercicio

--- exerc/data/script.js
+++ exerc/data/script.js
@@ -1,18 +1,19 @@
 function carregar() {
     var msg = window.document.getElementById('mensagem')
     var img = window.document.getElementById('foto')
+
     var data = new Date()
     //var hora = data.getHours()
-    var hora = 18
-    msg.innerHTML = `Agora são ${horas} horas`
+    var hora = data.getHours();
+    msg.innerHTML = `Agora são ${hora} horas`
 
     if (hora >= 0 && hora < 12) {
-        img.src = "imagens/manha.jpg"
+        img.style.backgroundImage = "url('imagens/manha.jpg')";
     }
     else if (hora >= 12 && hora < 18) {
-        img.src = "imagens/tarde.jpg"
+        img.style.backgroundImage = "url('imagens/tarde.jpg')";
     }
     else {
-        img.src = "imagens/noite.jpg"
+        img.style.backgroundImage = "url('imagens/noite.jpg')";
     }
 }
