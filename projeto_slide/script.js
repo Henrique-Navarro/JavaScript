71b217df88a71217e90ef40605f802a42dbdc0fb
Henrique-Navarro <henriquenavarromorais@gmail.com>
2022-03-16 03:06:09 -0300

    slide

---
+++ projeto_slide/script.js
@@ -0,0 +1,37 @@
+let slideIndex=1;
+showSlides(slideIndex);
+
+function plusSlides(n){
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n){
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n){
+  let i;
+  let slides= document.getElementsByClassName("imgs-background");
+  let dots= document.getElementsByClassName("dot");
+  
+  //tratar situações de soma/subtração das setinhas
+  if(n > slides.length){
+    slideIndex=1;
+  }
+  if(n < 1){
+    slideIndex=slides.length;
+  }
+  
+  //fazer todos os slides desaparecerem
+  for(i=0; i<slides.length; i++){
+    slides[i].style.display="none";
+  }
+  
+  //ativar slides conforme dot está selecionado
+  for(i=0; i< dots.length; i++){
+    dots[i].className= dots[i].className.replace(" active", "");
+  }
+  
+  slides[slideIndex-1].style.display= "block";
+  dots[slideIndex-1].className += " active";
+}
